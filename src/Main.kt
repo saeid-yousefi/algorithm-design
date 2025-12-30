7cb927b7f915cb159c8cb519dba800b7e82bc972
saeid-yousefi <saeid.yousefi.1373@gmail.com>
2025-12-30 12:33:11 +0330

    binary search added

--- src/Main.kt
+++ src/Main.kt
@@ -3,9 +3,9 @@ import searches.NormalSearch
 
 fun main() {
     runWithTimer("found with normal search in") {
-        println("item index=" + NormalSearch().find(dummySortedList, dummyItem))
+        println("item index = " + NormalSearch().find(dummySortedList, dummyItem))
     }
     runWithTimer("found with binary search in") {
-        println("item index=" + BinarySearch().find(dummySortedList, dummyItem))
+        println("item index = " + BinarySearch().find(dummySortedList, dummyItem))
     }
 }
